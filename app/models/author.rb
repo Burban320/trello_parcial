20875edaa58502d16a510a47b83b3608c4d450d8
Sebastian Burbano <seburbano@uninorte.edu.co>
2015-11-09 15:48:59 +0000

    Edit Author password validation fixed. Duration changed from end to current, to from end to created

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,8 +7,6 @@ class Author < ActiveRecord::Base
     
     validates :email, presence: true,
                       uniqueness: true
-                      
-    validates :password, presence: true
     
     validates :first_name, presence: true
     validates :last_name, presence: true
